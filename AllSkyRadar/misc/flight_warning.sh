8ad77e0ccd2fe8e37edaceffa23544027f4d5905
Spinki <23009891+spink-al@users.noreply.github.com>
2020-02-27 05:51:51 +0100

    Update flight_warning.sh

--- AllSkyRadar/misc/flight_warning.sh
+++ AllSkyRadar/misc/flight_warning.sh
@@ -5,7 +5,7 @@ cd ${FW_FLDR}
 
 pwd
 while true ; do 
-    nc -w 30 localhost 33333 | python flight_warning_M_CLEAN_CORR_MERGE2_nowrite.py
+    nc -w 30 localhost 33333 | python flight_warning_MLAT.py.py
     sleep 5s 
 done
 

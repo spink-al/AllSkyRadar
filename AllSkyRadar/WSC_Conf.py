82ec935ee443fb9a7d708245a6178bb9be5c3418
Spinki <23009891+spink-al@users.noreply.github.com>
2020-02-29 05:41:34 +0100

    Update WSC_Conf.py

--- AllSkyRadar/WSC_Conf.py
+++ AllSkyRadar/WSC_Conf.py
@@ -1,8 +1,48 @@
+# Need reset lvl 1
 # size of jpg's with overlay
-w_resize = "1920"
-h_resize = "1080"
+w_resize        = "1920"
+h_resize        = "1080"
+
 # resized jpg quality
-q_resize = "85"
-q_fullsize = "100"
+q_resize        = "85"
+q_fullsize      = "85"
+
 # Where the camera is pointing
-cam_azimuth = "262"
+cam_azimuth     = "105"
+
+# Crop fullsize (camera.resolution) 3104x2304 to 3104x1746 starting at x=0, y=0
+crop_x          = "0"
+crop_y          = "0"
+crop_w          = "3104"
+crop_h          = "1746"
+# plot enabled/disabled
+overlay         = "1"
+# plot frame:
+spines_ovrl     = "1"
+# aside from sun/moon/planets: 
+stars_ovrl      = "0"
+
+iss_ovrl        = "1" #todo
+
+calibration1_ovrl        = "1" #todo
+calibration2_ovrl        = "1" #todo
+calibration3_ovrl        = "1" #todo
+calibration4_ovrl        = "1" #todo?
+
+landmarks_ovrl  = "1" #todo
+# time costly but eyecandy plane trails:
+alphablend_trails = "1" #todo 
+
+
+# Hardware flips need script reset lvl2
+h_flip          = "1"
+v_flip          = "1"
+
+# White Balance d_ = day n_ = night
+d_wb1           = "1.7"
+d_wb2           = "1.4"
+n_wb1           = "1.2"
+n_wb2           = "2.0"
+
+
+

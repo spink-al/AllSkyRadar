5283abd65130d26d4cfe488183937668245ab67d
Spinki <23009891+spink-al@users.noreply.github.com>
2020-03-02 16:20:49 +0100

    Update start_dummy.sh

--- AllSkyRadar/start_dummy.sh
+++ AllSkyRadar/start_dummy.sh
@@ -3,7 +3,7 @@
 . ./ASR_Conf.py
 mkdir -p /tmp/AllSkyRadar/
 sudo ln -sf /tmp/AllSkyRadar/dummy_1080p.jpg /var/www/html/dummy_1080p.jpg
-sudo cp misc/dummy.html /var/www/html/ 
+sudo cp www/dummy.html /var/www/html/ 
 while true ; do 
     echo "START: "`date` 
     python3 ${BIN_FLDR}/dummy.py 

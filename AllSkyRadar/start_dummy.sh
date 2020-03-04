d1a6c41406a646460c335b8530bc502a6cd92295
Spinki <23009891+spink-al@users.noreply.github.com>
2020-03-04 02:07:22 +0100

    autodownload iss.tle

--- AllSkyRadar/start_dummy.sh
+++ AllSkyRadar/start_dummy.sh
@@ -2,6 +2,10 @@
 # Read config
 . ./ASR_Conf.py
 mkdir -p /tmp/AllSkyRadar/
+
+wget -qr https://www.celestrak.com/NORAD/elements/stations.txt -O /tmp/AllSkyRadar/stations.txt
+grep "ISS (ZARYA)" /tmp/AllSkyRadar/stations.txt -A 2 > /tmp/AllSkyRadar/iss.tle
+
 sudo ln -sf /tmp/AllSkyRadar/dummy_1080p.jpg /var/www/html/dummy_1080p.jpg
 sudo cp www/dummy.html /var/www/html/ 
 while true ; do 

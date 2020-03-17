73b212b975ff4a408ffd08e08825a65a3ff95a77
spink-al <lambea@o2.pl>
2020-03-17 05:51:37 +0100

    corr1

--- install/installer.sh
+++ install/installer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo cp -R html /var/www
 sudo mkdir /etc/raspap
-mkdir /var/www/html/html_wzzak_bak
+sudo mkdir /var/www/html/html_wzzak_bak
 
 sudo cp -R etc/* /etc/raspap
-
+sudo cp html_wzzak_bak/* /var/www/html/html_wzzak_bak
 sudo chown -R www-data:www-data /etc/raspap/
 sudo chown -R www-data:www-data /var/www/html/
 
@@ -17,9 +17,9 @@ sudo systemctl restart lighttpd.service
 sudo ln -sf /tmp/AllSkyRadar/dummy_1080p.jpg /var/www/html/dummy_1080p.jpg
 sudo ln -sf /tmp/AllSkyRadar/dummy_1080p.jpg /var/www/html/html_wzzak_bak/dummy_1080p.jpg
 
-sudo ln -sf /tmp/AllSkyRadar/out6.html /var/www/html/out6.html
-sudo ln -sf /tmp/AllSkyRadar/out6.html /var/www/html/html_wzzak_bak/out6.html
+sudo ln -sf /tmp/out6.html /var/www/html/out6.html
+sudo ln -sf /tmp/out6.html /var/www/html/html_wzzak_bak/out6.html
 
-sudo ln -sf /tmp/AllSkyRadar/out7.html /var/www/html/out7.html
-sudo ln -sf /tmp/AllSkyRadar/out7.html /var/www/html/html_wzzak_bak/out7.html
+sudo ln -sf /tmp/out7.html /var/www/html/out7.html
+sudo ln -sf /tmp/out7.html /var/www/html/html_wzzak_bak/out7.html
 
